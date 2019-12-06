40140051df67c264814e87fa9477745a6ee5cb3b
Nicole white <you@example.com>
2019-12-06 06:52:10 -1000

    topic tabs in

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,22 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function Tab(topic){
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = topic;
+    return tab;
+}
+
+document.querySelector('.topics').appendChild(Tab('all'));
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(data => {
+        data.data.topics.forEach(i => {
+            document.querySelector('.topics').appendChild(Tab(i));
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    
